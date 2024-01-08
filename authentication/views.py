37c2234c29be0eb558a24a44d48b97512c4badf1
coyote95 <marie.guillot.oc@gmail.com>
2024-01-08 21:50:37 +0100

    add inscription

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import logout, login
+from django.conf import settings
+from . import forms
 
 
 def logout_user(request):
     logout(request)
     return redirect("login")
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == "POST":
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form':form})
--- booksblog/urls.py
+++ booksblog/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
                                redirect_authenticated_user=True), name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
     path('home/', blog.views.home, name='home'),
+    path('signup/',authentication.views.signup_page, name='signup'),
 ]
